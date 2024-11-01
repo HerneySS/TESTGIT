6f98604e8c25531c9c022fe17db2303575116858
rc <rokke1558@gmail.com>
2024-11-01 12:01:53 -0500

    Nuevo proyecto de pruebas

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.querySelector('.icon-hamburguesa').addEventListener('click', function() {
+    let menu = document.querySelector('nav');
+    // Agregar o modificar atributo
+    if (!menu.hasAttribute('active')) {
+      // Agregar el atributo si no existe
+      menu.setAttribute('active', "");
+    } else {
+      // Hacer algo con el valor actual, por ejemplo, cambiarlo y actualizar el atributo
+      menu.removeAttribute('active');
+    }
+  });
+  
+  
+  
+  document.querySelector('.icon-close-menu').addEventListener('click', function() {
+    let menu = document.querySelector('nav');
+    // Agregar o modificar atributo
+    menu.removeAttribute('active');
+
+  });
